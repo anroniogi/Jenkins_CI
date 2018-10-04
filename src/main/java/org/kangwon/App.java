bc94e783a5000699708f92612637d272398613b6
ab3774 <35440752+ab3774@users.noreply.github.com>
2018-10-04 23:24:59 +0900

    Update App.java
    random2

--- src/main/java/org/kangwon/App.java
+++ src/main/java/org/kangwon/App.java
@@ -1,5 +1,6 @@
 package org.kangwon;
 import java.util.Random;
+
 /**
  * Hello world!
  *
@@ -12,11 +13,24 @@ public class App
         int r;
         r = random.nextInt(15);
 
-        for(int i=0;i<r;i++){
-            System.out.println(" \"KNU-7\" ");
-
+        for(int i=0; i<r; i++){
+            for(int j=0; j<i; j++) {
+                if((i + 1) % 2 == 1)
+                System.out.print(" \"KNU-7\" ");
+                else{
+                    System.out.print(" \" *** \" ");
+                }
+            }System.out.println(" ");
         }
-
-
+        for(int l=r;l>0;l--){
+            for(int j=0; j<l; j++) {
+                if((l + 1) % 2 == 1)
+                    System.out.print(" \"KNU-7\" ");
+                else{
+                    System.out.print(" \" *** \" ");
+                }
+            }System.out.println(" ");
+        }
+        System.out.println(" ");
     }
 }
