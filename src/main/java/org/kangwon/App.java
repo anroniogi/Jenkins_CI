d064679848e38386916dceaea71a19c616756981
ab3774 <35440752+ab3774@users.noreply.github.com>
2018-10-04 22:28:43 +0900

    Update App.java
    random

--- src/main/java/org/kangwon/App.java
+++ src/main/java/org/kangwon/App.java
@@ -4,10 +4,19 @@ package org.kangwon;
  * Hello world!
  *
  */
-public class App 
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "KNU-7" );
+        Random random = new Random();
+        int r;
+        r = random.nextInt(15);
+
+        for(int i=0;i<r;i++){
+            System.out.println(" \"KNU-7\" ");
+
+        }
+
+
     }
 }
